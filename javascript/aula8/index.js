a3e3ddb1d3872b03f0405bdc9df67f0a3970d04c
alves.dev <henriquefdalves15@gmail.com>
2025-03-11 23:40:33 -0300

    fiz um ex de let e const

---
+++ javascript/aula8/index.js
@@ -0,0 +1,11 @@
+const nome = 'Henrique';
+const sobrenome = 'Alves';
+const idade = 20;
+const peso = 70;
+const altura = 1.80;
+
+let imc = peso / (altura * altura);
+let anoNascimento = 2025 - idade;
+
+
+console.log(nome,sobrenome,'tem',idade,'anos pesa',peso,'Kg tem',altura,'de altura e seu IMC é de',imc,'nascido em',anoNascimento);
