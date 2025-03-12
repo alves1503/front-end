078beb4a1342e151d84ef3f4ad610c5177f82054
alves.dev <henriquefdalves15@gmail.com>
2025-03-11 23:47:29 -0300

    atualizei o commit anterior

--- javascript/aula8/index.js
+++ javascript/aula8/index.js
@@ -9,3 +9,5 @@ let anoNascimento = 2025 - idade;
 
 
 console.log(nome,sobrenome,'tem',idade,'anos pesa',peso,'Kg tem',altura,'de altura e seu IMC é de',imc,'nascido em',anoNascimento);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos pesa ${peso} Kg tem altura de ${altura} e seu IMC é de ${imc} nascido em ${anoNascimento}`);
