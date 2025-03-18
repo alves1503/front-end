0e7a8e076191bfd47cb67868842710e6d5fee745
alves.dev <henriquefdalves15@gmail.com>
2025-03-17 22:03:12 -0300

    aula10

---
+++ Udemy/javascript/aula10/index.js
@@ -0,0 +1,11 @@
+const nome = 'Henrique'; //string
+const nome1 = 'Henrique'; //string
+const nome2 = 'Henrique'; //string
+
+const num1 = 10; //number
+const num2 = 10.5; //number
+
+let nomeAluno; //undefined - não aponta para local nenhum da memoria
+let sobrenomeAluno = null; //Nulo - não aponta para local nenhum da memoria
+
+const boolean = true; // boolean = True ou FAlse
