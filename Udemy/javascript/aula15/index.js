500d4314199b7a35cb917fdb6962e970b6347063
alves.dev <henriquefdalves15@gmail.com>
2025-04-22 22:34:06 -0300

    aula sobre MAth

---
+++ Udemy/javascript/aula15/index.js
@@ -0,0 +1,12 @@
+console.log(100 / 0);
+//let num2 = Math.floor(num1); Arredonda para baixo
+//let num2 = Math.ceil(num1); Arredonda para cima
+//let num2 = Math.round(num1); arredonda para o mais proximo
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,+1500,9,7,8,6)); //Maior numero da sequenia
+//console.log(Math.min(1,2,3,4,5,-10,-50,+1500,9,7,8,6)); //Menor numero da sequenia
+
+//const aleatorio = Math.round(Math.random() * (10 -5) + 5);
+
+
+
