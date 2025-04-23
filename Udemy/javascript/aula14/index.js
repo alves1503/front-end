7e309038674df25fd8147e9d1e7b8743a546026a
alves.dev <henriquefdalves15@gmail.com>
2025-04-22 22:18:12 -0300

    aula 14

---
+++ Udemy/javascript/aula14/index.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let nem2 = 2.5;
+
+//console.log(num1.toString() +num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));
+//let temp = num1 * 'Ola';
