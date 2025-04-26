4143e245a12a6049a89bcbe89a315f1c46d0ebb3
alves.dev <henriquefdalves15@gmail.com>
2025-04-25 22:16:36 -0300

    objetos

---
+++ Udemy/javascript/aula18/index.js
@@ -0,0 +1,15 @@
+/* function criaPessoa (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade,
+    };
+}
+
+
+const pessoa1 = criaPessoa('João', 'Alves', 20);
+const pessoa2 = criaPessoa('Henrique', 'Alves', 20);
+const pessoa3 = criaPessoa('MArcos', 'Alves', 20);
+console.log(pessoa1.nome);
+console.log(pessoa2.nome);
+console.log(pessoa3.nome); */
