5ff6b914ff3b5d84f57076e618aed25bc70d7b46
alves.dev <henriquefdalves15@gmail.com>
2025-04-25 21:44:35 -0300

    Create index.js

---
+++ Udemy/javascript/aula17/index.js
@@ -0,0 +1,12 @@
+/* function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(2, 2);
+console.log(resultado); */
+
+const raiz = (n) => n ** 0.5;
+
+
+console.log(raiz(9));
