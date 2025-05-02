a0f1913303da4e0503ba49a3a167f603dac0c17e
alves.dev <henriquefdalves15@gmail.com>
2025-05-01 23:19:06 -0300

    objets date

---
+++ Udemy/javascript/02 - Js Logica de programação/aula0027/index.js
@@ -0,0 +1,11 @@
+const pontuacao = 999;
+const usuario = pontuacao >= 1000 ? 'Usuario Vip' : 'Usuario Normal';
+
+console.log(usuario);
+
+
+/* if (pontuacao >= 1000) {
+    console.log('Usuario VIP');
+} else {
+    console.log('Usuario Normal');
+} */
---
+++ Udemy/javascript/02 - Js Logica de programação/aula0028/index.js
@@ -0,0 +1,18 @@
+/* const data = new Date('2019-04-20 20:20:59');
+console.log('Dia', data.getDate());
+console.log('Mes', data.getMonth() + 1);
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Minuto', data.getMinutes());
+console.log('Segundo', data.getSeconds());
+console.log('Milissegundo', data.getMilliseconds());
+console.log('Dia da semana', data.getDay());
+console.log(data.toString()); */
+
+
+function formatData (data) {
+}
+
+const data = new Date();
+const dataBrasil = formatData(data) ;
+console.log(dataBrasil);
