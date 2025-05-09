0f7e6fa18eeaaf278b3e6803c64df7ae3d19d021
alves.dev <henriquefdalves15@gmail.com>
2025-05-08 23:09:09 -0300

    att

--- Udemy/javascript/02 - Js Logica de programação/aula0030/index.js
+++ Udemy/javascript/02 - Js Logica de programação/aula0030/index.js
@@ -1,25 +1,5 @@
-let n1 = prompt('Digite a primeira nota:');
-let n2 = prompt('Digite a segunda nota:');
-let n3 = prompt('Digite a terceira nota:');
-
-n1 = parseFloat(n1);
-n2 = parseFloat(n2);
-n3 = parseFloat(n3);
-
-const media = (n1 + n2 + n3) / 3;
-
-if (media >= 6) {
-    console.log('Aprovado')
-} else {
-    console.log('Reprovado')
-}
-
-
-
-
-
-
-
+let nome = 'Henrique';
+var nome2 = 'Henrique';
 
 
 
