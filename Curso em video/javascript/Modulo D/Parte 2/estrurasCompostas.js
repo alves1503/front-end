65cc1abfc0ee02a756950bff03ac7d0878b330d6
alves.dev <henriquefdalves15@gmail.com>
2025-05-14 22:14:29 -0300

    fiz a parte 2 de condições

---
+++ Curso em video/javascript/Modulo D/Parte 2/estrurasCompostas.js
@@ -0,0 +1,9 @@
+var idade = 70
+
+if (idade >= 16 && idade < 18 || idade > 65) {
+    console.log('Você vota (opcional)')
+} else if (idade > 18) {
+    console.log('Você vota')
+} else {
+    console.log('Você não vota')
+}
