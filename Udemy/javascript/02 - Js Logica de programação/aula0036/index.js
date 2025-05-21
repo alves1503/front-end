d46cedf88c5d778eed664885b06a5c3a039174ea
alves.dev <henriquefdalves15@gmail.com>
2025-05-20 23:18:35 -0300

    Criei um ex com For in

---
+++ Udemy/javascript/02 - Js Logica de programação/aula0036/index.js
@@ -0,0 +1,16 @@
+/* const frutas = ['Pera', 'Maçã', 'Uva']
+
+for (let i in frutas) {
+    console.log(frutas[i])
+} */
+
+    const pessoas = {
+        nome: 'Henrique',
+        sobrenome: 'Alves',
+        idade: 20
+    }
+
+
+    for (let i in pessoas){
+        console.log(pessoas[i])
+    }
