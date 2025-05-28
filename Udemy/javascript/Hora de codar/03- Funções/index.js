1dd734ec28914a8141394a7527ba0950343aac63
alves1503 <henriquefdalves15@gmail.com>
2025-05-28 20:51:40 -0300

    Retorno de valores função

--- Udemy/javascript/Hora de codar/03- Funções/index.js
+++ Udemy/javascript/Hora de codar/03- Funções/index.js
@@ -16,3 +16,17 @@ function comParametro (txt) {
     console.log(`Impreimindo : ${txt}`)
 }
 comParametro("Imprimindo algo")
+
+
+// 02- RETORNNDO VALORES DA FUNÇÃO
+const a = 2
+const b = 20
+const c = 10
+const d = 25
+
+function somaValores (a,b) {
+    return a + b
+}
+
+const res = somaValores(a,b)
+console.log(res)
