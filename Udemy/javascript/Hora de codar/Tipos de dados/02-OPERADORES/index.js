a9cb48f24e98938104ecf8a57a40b14aa9fa72b1
alves1503 <henriquefdalves15@gmail.com>
2025-05-27 22:16:16 -0300

    Terminei os estudos relacionados a tipos de dados

--- Udemy/javascript/Hora de codar/Tipos de dados/02-OPERADORES/index.js
+++ Udemy/javascript/Hora de codar/Tipos de dados/02-OPERADORES/index.js
@@ -4,3 +4,33 @@ console.log(typeof 2)
 // 02 OPS-ARITIMETRICAS
 console.log(2+(2*2))
 console.log(9*9)
+
+// 03 SPECIAL-NUMBERS
+console.log(typeof Infinity)
+
+// 04 STRINGS
+console.log('Trabalhando com strings')
+
+// 05 SIMBOLOS ESPECIAIS COM STRINGS
+console.log('Testando a \n quebra de linha')
+
+console.log(`Espaçamento \t de tab`)
+
+// 06 CONCATENAÇÃO
+console.log('Oi ' + 'Tudo bem?')
+
+// 07 INTERPOLAÇÃO
+console.log(`A conta de 2 + 2 é: ${2 + 2}`)
+
+// 08 BOOLEANS
+console.log(2 == 8)
+
+
+// 09 COMPARAÇÕES 
+console.log(8 > 5)
+console.log(5 > 8)
+console.log(5 == 5)
+console.log(5 === 5)
+console.log(8 != 5)
+
+
