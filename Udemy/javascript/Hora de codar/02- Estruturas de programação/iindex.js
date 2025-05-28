3b9ae19ed46b014fbea91daf61950ac80201850d
alves1503 <henriquefdalves15@gmail.com>
2025-05-28 00:33:46 -0300

    Finalizei a parte de estruturas de programação

---
+++ Udemy/javascript/Hora de codar/02- Estruturas de programação/iindex.js
@@ -0,0 +1,43 @@
+// 01 VARIAVEIs
+let nome = 'Henrique';
+console.log(nome)
+
+
+const teste = 'Não muda de valor'
+console.log(teste)
+
+let a = 10,b = 20,c = 30;
+console.log(a, b, c) 
+
+
+// 02 FUNÇÕES
+/* alert('Voçê vai reber pedido de informação posteriormente')
+const age = prompt('Digite sua idade:')
+console.log(`Voçê tem ${age} anos`) */
+
+
+//05 OBJETOS MATH
+console.log(Math.max(2,5,8,10))
+console.log(Math.floor(10.5))
+
+// 06 CONSOLE.X
+console.log('TEste!')
+console.error('Erro')
+console.warn('Aviso')
+
+// ESTRUTURA CONDICONAL IF
+const m = 9
+
+if (m > 10) {
+    console.log('Seu numero é maior que 10')
+} else {
+    console.log('Seu numero é menor que 10')
+}
+    
+//ESTRUTURA DE REPETIÇÃO WHILE
+let p = 0
+
+while (p < 5) {
+    console.log(`Seu numero é: ${p}`)
+    p++
+}
