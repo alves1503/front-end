e3e4d2c54436954a3ceff64bc6d266c08000281b
alves1503 <henriquefdalves15@gmail.com>
2025-05-28 23:30:21 -0300

    Mais sobre arrays e suas Propriedades

--- Udemy/javascript/Hora de codar/04- Arrays e Objetos/index.js
+++ Udemy/javascript/Hora de codar/04- Arrays e Objetos/index.js
@@ -2,3 +2,19 @@
 const list = [1, 2, 3, 4, 5]    
 
 console.log(list)
+
+// 02- MAIS SOBRE ARRAYS 
+const arr = ['a', 'b']
+
+console.log(arr[0])
+
+// 03- PROPRIEDADES 
+const numbers = [5, 4, 8]
+
+
+console.log(numbers.length)
+console.log(numbers["length"])
+
+const myName = 'Henrique'
+
+console.log(myName.length)
