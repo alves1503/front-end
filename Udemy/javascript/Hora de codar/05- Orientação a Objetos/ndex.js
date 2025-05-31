6b041ed02079ab88fddd9e12aa118f7ccf1e2a01
alves1503 <henriquefdalves15@gmail.com>
2025-05-31 11:51:28 -0300

    Metodos

--- Udemy/javascript/Hora de codar/05- Orientação a Objetos/ndex.js
+++ Udemy/javascript/Hora de codar/05- Orientação a Objetos/ndex.js
@@ -1,8 +1,8 @@
-// METODOS
+// 01- METODOS
 const animal = {
     nome: 'Bob',
-    
-    latir: function() {
+
+    latir: function () {
         console.log('Au Au Au')
     }
 }
@@ -10,3 +10,24 @@ const animal = {
 console.log(animal.nome)
 
 animal.latir()
+
+// 02- APROFUNDANDO METODOS
+const pessoa = {
+    nome: 'Henrique',
+
+    getNome: function () {
+        return this.nome
+    },
+
+        setNome: function (novoNome) {
+        this.nome = novoNome
+    }
+}
+
+console.log(pessoa.nome)
+
+console.log(pessoa.getNome())
+
+pessoa.setNome('Joaquim')
+
+console.log(pessoa.getNome())
