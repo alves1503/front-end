e47fa11c07d41c55ca2d47e0dad98b7e3f62779d
alves1503 <henriquefdalves15@gmail.com>
2025-06-01 01:09:36 -0300

    Console.log

--- Udemy/javascript/Hora de codar/06- Depuração com JavaScript/index.js
+++ Udemy/javascript/Hora de codar/06- Depuração com JavaScript/index.js
@@ -3,3 +3,25 @@
 'use strict'
 
 const opa = 'teste'
+
+// 2- COONSOLE.LOG
+
+let a = 1
+let b = 2
+
+if (a == 1) {
+    a = b + 2
+}
+
+console.log(a)
+
+for (let i =0; i < b; i++) {
+    a = a + 2
+    console.log(a)
+}
+
+if (a > 5){
+    a = 25
+}
+
+console.log(a)
