ff5b0c57c91c689358c093569268e2d4ce5fc80b
alves1503 <henriquefdalves15@gmail.com>
2025-06-02 00:27:43 -0300

    Async Awaits

--- Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
+++ Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
@@ -93,3 +93,21 @@ somarComDelay(2,4).then((value) => {
 })
 
 console.log('Teste asnc')
+
+// 8 -ASYNC AWAIIT
+function resolveComDelay(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('Resolveu a promise')
+        },2000)
+    })
+}
+
+async function chamadaAsync() {
+    console.log('Chamando a promise, e esperando o resultado') 
+    const result = await resolveComDelay()
+    console.log(`O resultado chegou: ${result}`)
+}
+
+
+chamadaAsync()
