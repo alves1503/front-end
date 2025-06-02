cc2d3875d89326f3f7d2210fe04232ae9a2f1810
alves1503 <henriquefdalves15@gmail.com>
2025-06-01 23:28:01 -0300

    setInterval

--- Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
+++ Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
@@ -1,4 +1,4 @@
-//SETTIMEOUT
+// 1- SETTIMEOUT
 console.log('Ainda não foi executada');
 
 setTimeout(function(){
@@ -6,6 +6,18 @@ setTimeout(function(){
     console.log('Executado!')
 
 }, 2000)
+
+
+
+console.log('Ainda não foi executada 2')
+
+// 2- SETINTERVAL
+console.log('Ainda não foi executada')
+
+
+setInterval(function() {
+    console.log('Intervalo assincrono')
+},2000)
 
 
 
