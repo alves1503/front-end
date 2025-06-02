24b61b020091171066e9dc3c81f59416c13121d3
alves1503 <henriquefdalves15@gmail.com>
2025-06-02 00:34:07 -0300

    Generator

--- Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
+++ Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
@@ -111,3 +111,20 @@ async function chamadaAsync() {
 
 
 chamadaAsync()
+
+
+// 9- GENERATOR
+
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const gen = generator()
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
