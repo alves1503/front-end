e10f2b2ce4e18fd4a9654eb435ab2e36f866e0c0
alves1503 <henriquefdalves15@gmail.com>
2025-06-01 23:58:23 -0300

    Reject

--- Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
+++ Udemy/javascript/Hora de codar/07- Programação Assincrona/index.js
@@ -24,7 +24,7 @@ setInterval(function() {
 console.log('Ainda não foi executada 2') */
 
 // 3- PROMISE
-const promessa = Promise.resolve(5 + 5) 
+const promessa = Promise.resolve(5 + 5)
 
 console.log('Algum código')
 
@@ -37,9 +37,27 @@ console.log('Outro código')
 
 // 4- FALHA NA PROMISE
 Promise.resolve(4 * 'asd')
-.then((n) => {
-    if(Number.isNaN(n)) {
-        throw new Error('VAlores invalidos')
-    }
-})
-.catch((err) => console.log(`Um erro ocorreu: ${err}`))
+    .then((n) => {
+        if (Number.isNaN(n)) {
+            throw new Error('VAlores invalidos')
+        }
+    })
+    .catch((err) => console.log(`Um erro ocorreu: ${err}`))
+
+// 5- REJEIÇÃO
+function checkNumber(n) {
+    return new Promise((resolve, reject) => {
+        if (n > 10) {
+            resolve(`O numero é maior que 10`)
+        } else {
+            reject(new Error("Numero muito baixo"))
+        }
+    })
+}
+
+const a = checkNumber(20)
+const b = checkNumber(10)
+
+a.then((v) => console.log(`O resultado é ${v}`)).catch((err) =>
+    console.log(`Um erro ocorreu: ${err}`)
+);
