2d11639d3281006a59b9275af912d646ae67fd97
alves1503 <henriquefdalves15@gmail.com>
2025-06-03 20:50:21 -0300

    Seleção por id

--- Udemy/javascript/Hora de codar/08- JavaScript e o Navegador/index.js
+++ Udemy/javascript/Hora de codar/08- JavaScript e o Navegador/index.js
@@ -1,3 +1,8 @@
 // 02- SELECIONANDO POR TAGS'
 const listItem = document.getElementsByTagName('li')
 console.log(listItem)
+
+
+// 03- SELECIONANDO ELEMENTOS POR ID
+const titulo = document.getElementById('title')
+console.log(titulo)
