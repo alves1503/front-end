5aa111f9cff1f15b9ff76d0db43622b4b2aac933
alves1503 <henriquefdalves15@gmail.com>
2025-06-04 20:51:12 -0300

    Removendo eventos

--- Udemy/javascript/Hora de codar/09- Eventos/js/index.js
+++ Udemy/javascript/Hora de codar/09- Eventos/js/index.js
@@ -4,3 +4,23 @@ const btn = document.querySelector('#myButton')
 btn.addEventListener('click', function () {
     console.log('Clicou aqui')
 })
+
+
+
+// 02- REMOVENDO EVENTOS
+const secondButton = document.querySelector('#btn')
+
+function imprimiButton() {
+    console.log('Teste')
+}
+
+secondButton.addEventListener('click', imprimiButton)
+
+// Outro botão //
+
+const thirdButton = document.querySelector('#otherBtn') 
+
+thirdButton.addEventListener('click', () => {
+    console.log('Evento removido')
+    secondButton.removeEventListener('click', imprimiButton)
+})
