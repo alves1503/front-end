01c18cbaac4ec95c2f3a50661d78749f9de8d8a7
alves1503 <henriquefdalves15@gmail.com>
2025-06-03 21:18:31 -0300

    InsertBefore

--- Udemy/javascript/Hora de codar/08- JavaScript e o Navegador/index.js
+++ Udemy/javascript/Hora de codar/08- JavaScript e o Navegador/index.js
@@ -16,3 +16,15 @@ console.log(navegacao)
 // 05- SELECIONANDO ELEMENTOS POR QUERY SELECTOR
 const navegacao2 = document.querySelectorAll('.nav')
 console.log(navegacao2)
+
+
+// 06- INSERTBEFORE
+
+// A primeira linha cria um novo elemento <p> (parágrafo) na página
+const p = document.createElement('p')
+
+// Aqui, estamos pegando o **elemento pai** do título. O "elemento pai" é o "responsável" pelo título, como uma caixa que contém o título.
+const header = titulo.parentElement
+
+// Agora, colocamos o novo parágrafo <p> dentro do "header" (caixa) antes do título "titulo".
+header.insertBefore(p, titulo)
