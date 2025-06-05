532b52b90f72cc01a90706a913e23105979b7121
alves1503 <henriquefdalves15@gmail.com>
2025-06-04 22:10:15 -0300

    Terminei a seção de eventos em js

--- Udemy/javascript/Hora de codar/09- Eventos/js/index.js
+++ Udemy/javascript/Hora de codar/09- Eventos/js/index.js
@@ -119,3 +119,25 @@ inputFocus.addEventListener('blur', (e) => {
     console.log('Saiu no input')
 
 })
+
+
+// 12- DEBOUNCE 
+const debounce = (f, delay) => {
+
+let timeout
+
+
+return (...arguments) => {
+    if(timeout) {
+        clearTimeout(timeout)
+        }
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        }, delay)
+    }
+}
+
+window.addEventListener('mousemove', debounce(() => {
+    console.log('Executando a cada 400ms')
+}, 400)
+)
