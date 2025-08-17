f91b78446ec208b7b66c38de7918bbad39f3b8e0
alves1503 <henriquefdalves15@gmail.com>
2025-08-17 01:09:06 -0300

    Fechei a seção de if e switch em javascript, santander

---
+++ Santander/JavaScript/03- Condições/index.js
@@ -0,0 +1,27 @@
+const idade = 17
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+} else {
+    console.log("Menor de idade")
+}
+
+
+
+let fruta = "uva"
+
+switch (fruta) {
+    case "laranja":
+        console.log("Suco de laranja")
+        break
+
+    case "banana":
+        console.log("Vitamina de banana")
+        break
+
+    case "maçã":
+        console.log("Suco de maçã") 
+        break
+    default:
+        console.log("Fruta invalida")
+}
