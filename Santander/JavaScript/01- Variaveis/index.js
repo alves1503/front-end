47df3c19cee414f12c58cb1e3aa410ab4fa0a13b
alves1503 <henriquefdalves15@gmail.com>
2025-08-17 00:40:10 -0300

    Iniciei os estudos em JavaScript, Santander

---
+++ Santander/JavaScript/01- Variaveis/index.js
@@ -0,0 +1,9 @@
+//nome, idade, numero de telefone, cpf, endereço
+
+let nomeDaVovo = "Maria"
+let idadeDaVovo = 60
+let numeroDeTelefone = "(37) 99999-9999"
+let cpf = "123456789-10"
+let endereco = "Rua alguma coisa"
+
+console.log(`Nome da vovó ${nomeDaVovo}, idade da vovó ${idadeDaVovo}, telefone da vovó ${numeroDeTelefone}, cpf ${cpf}, endereço ${endereco}`)
---
+++ Santander/JavaScript/02- arrays e vetores/index.js
@@ -0,0 +1,11 @@
+const onePiece = ["Luffy", "Zoro", "Nami"]
+onePiece.push("Robin")
+
+console.log(onePiece[3])
+
+let marinha = [
+    ["Garp", 70],
+    ["Kobe", 18]
+]
+
+console.log(marinha[1][0])
