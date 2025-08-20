ae13082b9f5b65115df34ee53cab46b796bc82b8
alves1503 <henriquefdalves15@gmail.com>
2025-08-20 03:44:49 -0300

    Criei a pasta para exercicios de livros, fixz o exercicio de medias

---
+++ Livros/Logica de programação com javascript/Exercicios/4- notas-aluno/index.js
@@ -0,0 +1,6 @@
+const primeiraNota = Number(prompt("Digite sua primeira nota: "));
+const segundaNota = Number(prompt("Digite sua segunda nota: "));
+
+const media = (primeiraNota + segundaNota) / 2;
+
+console.log(`Sua media é de: ${media}`);
