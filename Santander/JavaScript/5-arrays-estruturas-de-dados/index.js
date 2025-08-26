160da0735b770a9a3e6a0f8d195dc8c52a81ca3c
alves1503 <henriquefdalves15@gmail.com>
2025-08-25 23:35:38 -0300

    Fiz um ex com for de array, Santander

--- Santander/JavaScript/5-arrays-estruturas-de-dados/index.js
+++ Santander/JavaScript/5-arrays-estruturas-de-dados/index.js
@@ -1,6 +1,23 @@
-const caixaDeBrinquedos = ["Bola", "Boneca", "Peteca"];
+/* const caixaDeBrinquedos = ["Bola", "Boneca", "Peteca"];
 
 console.log(caixaDeBrinquedos);
 
-caixaDeBrinquedos.push("Carrinho de controle remoto")
-console.log(caixaDeBrinquedos);
+caixaDeBrinquedos.push("Carrinho de controle remoto");
+console.log(caixaDeBrinquedos); */
+
+const notas = [];
+
+notas.push(5);
+notas.push(4);
+notas.push(8);
+notas.push(4);
+notas.push(9);
+
+let soma = 0;
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i]
+    soma = soma + nota;
+}
+
+const mediaNotas = soma / notas.length;
+console.log(mediaNotas);
