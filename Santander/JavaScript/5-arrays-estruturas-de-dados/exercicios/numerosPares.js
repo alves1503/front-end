5c16118de5d6f52cee91ed0c76d4379782cddd6e
alves1503 <henriquefdalves15@gmail.com>
2025-08-25 23:46:42 -0300

    Finalizei  o exercicio de numeros pares, Santander

--- Santander/JavaScript/5-arrays-estruturas-de-dados/exercicios/numerosPares.js
+++ Santander/JavaScript/5-arrays-estruturas-de-dados/exercicios/numerosPares.js
@@ -1,3 +1,11 @@
 //Crie um programa que percorre uma lista de numeros e imprima cada numero par
 
-const numeros = [2, 5, 8, 7, 4]
+const numeros = [2, 5, 8, 7, 4];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    console.log(`O numero ${numeros[i]}  é par`);
+  }/*  else {
+    console.log(`O numero ${numeros[i]}  é impar`);
+  } */
+}
