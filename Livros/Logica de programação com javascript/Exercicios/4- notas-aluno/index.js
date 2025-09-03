08b4b725f969badc791e7ac45c09fcd4b0efb580
alves1503 <henriquefdalves15@gmail.com>
2025-09-03 02:55:27 -0300

    Organizei o repo

--- Livros/Logica de programação com javascript/Exercicios/4- notas-aluno/index.js
+++ Livros/Logica de programação com javascript/Exercicios/4- notas-aluno/index.js
@@ -2,5 +2,5 @@ const primeiraNota = Number(prompt("Digite sua primeira nota: "));
 const segundaNota = Number(prompt("Digite sua segunda nota: "));
 
 const media = (primeiraNota + segundaNota) / 2;
-
+    
 console.log(`Sua media é de: ${media}`);
