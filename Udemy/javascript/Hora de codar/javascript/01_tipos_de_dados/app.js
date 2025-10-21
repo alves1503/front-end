2493caa93036c03e79daf6a1512c098019cf6be0
alves1503 <henriquefdalves15@gmail.com>
2025-10-20 23:35:46 -0300

    Terminei o tipo de dados em js

---
+++ Udemy/javascript/Hora de codar/javascript/01_tipos_de_dados/app.js
@@ -0,0 +1,14 @@
+const n1 = 10;
+const nome = "Henrique";
+const boole = true;
+
+const pessoa = {
+  nome: "Henrique",
+  idade: 20,
+  profissao: "dev",
+};
+
+console.log(typeof n1);
+console.log(typeof nome);
+console.log(typeof boole);
+console.log(typeof pessoa);
