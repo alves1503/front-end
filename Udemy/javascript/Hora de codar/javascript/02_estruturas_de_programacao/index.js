b2f47b2312a382d73e1637ff936416006c798c60
alves1503 <henriquefdalves15@gmail.com>
2025-10-21 21:57:07 -0300

    Terminei a parte de estruturas de programação em js

---
+++ Udemy/javascript/Hora de codar/javascript/02_estruturas_de_programacao/index.js
@@ -0,0 +1,13 @@
+// 01- Variaveis
+let variavel1 = "Variavel que pode ser reatribuida seu valor";
+const variavel2 = "Variavel quetem valor unico, não pode ser reatribuida";
+
+// 03 - Função
+/* const x = prompt('Digite seu nome: ');
+alert(`Ola ${x}`); */
+
+
+variavel1 = 50,545;
+console.log(Math.round(variavel1,1))
+
+
